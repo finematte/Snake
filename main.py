9b1e2b367a0941bdb144aebde44cfc937941ef1e
matte <mattemill@protonmail.com>
2022-04-10 01:15:45 +0200

    Snake movement.

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ class Snake:
         self.color = BLUE
         self.headX = random.randrange(0, WIDTH, PIXELS)
         self.headY = random.randrange(0, HEIGHT, PIXELS)
+        self.bodies = []
         self.state = "STOP"  # STOP, UP, DOWN, RIGHT,
 
-    def move(self):
+    def move_head(self):
         if self.state == "UP":
             self.headY -= PIXELS
         elif self.state == "DOWN":
@@ -34,8 +35,25 @@ class Snake:
         elif self.state == "RIGHT":
             self.headX += PIXELS
 
+    def move_body(self):
+        if len(self.bodies) > 0:
+            for i in range(len(self.bodies) - 1, -1, -1):
+                if i == 0:
+                    self.bodies[0].posX = self.headX
+                    self.bodies[0].posY = self.headY
+                else:
+                    self.bodies[i].posX = self.bodies[i-1].posX
+                    self.bodies[i].posY = self.bodies[i-1].posY
+
+    def add_body(self):
+        body = Body(BLUE, self.headX, self.headY)
+        self.bodies.append(body)
+
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, (self.headX, self.headY, PIXELS, PIXELS))
+        if len(self.bodies) > 0:
+            for body in self.bodies:
+                body.draw(surface)
 
 
 class Apple:
@@ -64,11 +82,21 @@ class Background:
                     counter += 1
 
 
-class Colission:
+class Collision:
 
     def between_snake_and_apple(self, snake, apple):
         distance = math.sqrt(math.pow((snake.headX - apple.posX), 2) + math.pow((snake.headY - apple.posY), 2))
         return distance < PIXELS
+
+
+class Body:
+    def __init__(self, color, posX, posY):
+        self.color = color
+        self.posX = posX
+        self.posY = posY
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.posX, self.posY, PIXELS, PIXELS))
 
 
 if __name__ == '__main__':
@@ -80,7 +108,12 @@ if __name__ == '__main__':
     apple = Apple()
     snake = Snake()
     background = Background()
-    collision = Colission()
+    collision = Collision()
+
+    # Updating screen
+    clock = pygame.time.Clock()
+    screen_update = pygame.USEREVENT
+    pygame.time.set_timer(screen_update, 150)
 
     # Main loop
     while True:
@@ -91,6 +124,9 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == screen_update:
+                snake.move_body()
+                snake.move_head()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     snake.state = "UP"
@@ -105,9 +141,8 @@ if __name__ == '__main__':
 
         if collision.between_snake_and_apple(snake, apple):
             apple.spawn()
-            # Increase the length of the snake
+            snake.add_body()
             # Increase the score
 
-        pygame.time.delay(80)
-        snake.move()
         pygame.display.update()
+        clock.tick(60)
